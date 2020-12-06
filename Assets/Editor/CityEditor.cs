cf6eb282051b333ad6c89c7b194ea35e69cd3595
Renato <renatofelixmartins@gmail.com>
2020-12-06 00:21:15 -0300

    prefabs build

--- Assets/Editor/CityEditor.cs
+++ Assets/Editor/CityEditor.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
 
-[CustomEditor(typeof(GenerateCity))]
 public class CityEditor : Editor
 {
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        GenerateCity city = target as GenerateCity;
+    //public override void OnInspectorGUI()
+    //{
+    //    base.OnInspectorGUI();
+    //    GenerateCity city = target as GenerateCity;
 
-    }
+    //}
 
 }
--- Assets/Scripts/BuildCity/GenerateCity.cs
+++ Assets/Scripts/BuildCity/GenerateCity.cs
@@ -4,71 +4,81 @@ using UnityEngine;
 using System.IO;
 using System.Text.RegularExpressions;
 
-[ExecuteInEditMode]
-public class GenerateCity : MonoBehaviour
+
+namespace Game
 {
-    public Vector2Int size;
-    [SerializeField] private CityBuilds cityBuilds;
-
-    private List<int> positionBuildText =  new List<int>();
-    private void Awake()
+    public class GenerateCity : MonoBehaviour
     {
-        convertTextToTile();
-    }
-    private void Start()
-    {
-        GenerateMap();
-    }
-
-    private void Update()
-    {
-        GenerateMap();
-    }
+        public Vector2Int size;
 
 
+        [SerializeField] private City city;
+        [SerializeField] private CityBuilds cityBuilds;
 
-    public void GenerateMap()
-    {
-        int a = 0;
-
-        string holderName = "Generated Map";
-
-        if (transform.Find(holderName))
+        private List<int> positionBuildText = new List<int>();
+        private void Awake()
         {
-            DestroyImmediate(transform.Find(holderName).gameObject);
+            convertTextToTile();
+        }
+        private void Start()
+        {
+            GenerateMap();
         }
 
-        Transform mapHolder = new GameObject(holderName).transform;
-        mapHolder.parent = transform;
-
-        for (int i = 0; i < size.y; i++)
+        private void Update()
         {
-            for (int y = 0; y < size.x; y++)
+            GenerateMap();
+        }
+
+
+
+        public void GenerateMap()
+        {
+            int a = 0;
+
+            string holderName = "Generated Map";
+
+            if (transform.Find(holderName))
             {
-                GameObject.Instantiate( cityBuilds.builds[positionBuildText[a]], new Vector3(i*2, 0, y*2), Quaternion.identity, mapHolder);
-                a++;
+                DestroyImmediate(transform.Find(holderName).gameObject);
             }
+
+            Transform mapHolder = new GameObject(holderName).transform;
+            mapHolder.parent = transform;
+
+            size.x = city.gridWidth;
+            size.y = city.gridHeight;
+
+            for (int i = 0; i < size.y; i++)
+            {
+                for (int y = 0; y < size.x; y++)
+                {
+                    GameObject obj =  GameObject.Instantiate(cityBuilds.builds[positionBuildText[a]], new Vector3(i * 2, 0, y * 2), Quaternion.identity, mapHolder);
+                    city.AddStructure(obj.GetComponent<Structure>(), new Vector2Int(i, y));
+                    a++;
+                }
+            }
+
         }
 
-    }
-
-    private void convertTextToTile()
-    {
-        string path = "Assets/Resources/MapDataDefault.txt";
-        string[] stringNumber;
-        //Read the text from directly from the test.txt file
-        StreamReader reader = new StreamReader(path);
-
-        stringNumber = reader.ReadToEnd().Split(' ','\n');
-
-        foreach (var item in stringNumber)
+        private void convertTextToTile()
         {
-            int number;
-            int.TryParse(item, out number);
-            positionBuildText.Add(number);
+            string path = "Assets/Resources/MapDataDefault.txt";
+            string[] stringNumber;
+            //Read the text from directly from the test.txt file
+            StreamReader reader = new StreamReader(path);
+
+            stringNumber = reader.ReadToEnd().Split(' ', '\n');
+
+            foreach (var item in stringNumber)
+            {
+                int number;
+                int.TryParse(item, out number);
+                positionBuildText.Add(number);
+            }
+            reader.Close();
         }
-        reader.Close();
+
+
     }
-
-
 }
