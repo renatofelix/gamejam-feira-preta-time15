6e1cc839b9658cb45c216a51675c1f7cc4a0ab1b
Renato <renatofelixmartins@gmail.com>
2020-12-06 19:51:15 -0300

    new prefabs

---
+++ Assets/Scripts/Camera/CameraInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class CameraInteraction : MonoBehaviour
+    {
+        public static Vector2Int GridMousePostion;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && !GridManager.Instance.isContructMode())
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit; 
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    GridManager.Instance.SetCurrentTile(hit.transform.GetComponent<Structure>()); 
+                }
+            }
+            else if(Input.GetMouseButtonDown(0) && GridManager.Instance.saleMode)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    GridManager.Instance.SaleStructure(hit.transform.GetComponent<Structure>());
+                }
+            }
+            else
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    GridMousePostion = new Vector2Int((int)(hit.transform.position.x / 2), (int)hit.transform.position.z / 2);
+                }
+            }
+        }
+    }
+}
