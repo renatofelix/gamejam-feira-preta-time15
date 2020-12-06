45947d26f1727a75055170922a4deeff6d81fd2d
Renato <renatofelixmartins@gmail.com>
2020-12-05 23:15:22 -0300

    models

---
+++ Assets/Scripts/BuildCity/GenerateCity.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+
+[ExecuteInEditMode]
+public class GenerateCity : MonoBehaviour
+{
+    public Vector2Int size;
+    [SerializeField] private CityBuilds cityBuilds;
+
+    private List<int> positionBuildText =  new List<int>();
+    private void Awake()
+    {
+        convertTextToTile();
+    }
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    private void Update()
+    {
+        GenerateMap();
+    }
+
+
+
+    public void GenerateMap()
+    {
+        int a = 0;
+
+        string holderName = "Generated Map";
+
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for (int i = 0; i < size.y; i++)
+        {
+            for (int y = 0; y < size.x; y++)
+            {
+                GameObject.Instantiate( cityBuilds.builds[positionBuildText[a]], new Vector3(i*2, 0, y*2), Quaternion.identity, mapHolder);
+                a++;
+            }
+        }
+
+    }
+
+    private void convertTextToTile()
+    {
+        string path = "Assets/Resources/MapDataDefault.txt";
+        string[] stringNumber;
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+
+        stringNumber = reader.ReadToEnd().Split(' ','\n');
+
+        foreach (var item in stringNumber)
+        {
+            int number;
+            int.TryParse(item, out number);
+            positionBuildText.Add(number);
+        }
+        reader.Close();
+    }
+
+
+}
---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float maxValueX;
+    [SerializeField] private float minValueX;
+    [SerializeField] private float maxValueY;
+    [SerializeField] private float minValueY;
+    [SerializeField] private float minValueZ;
+    [SerializeField] private float maxValueZ;
+
+
+    [Header("Setting Camera")]
+    [SerializeField] private float speedCamera;
+
+
+    [SerializeField] private Camera camera;
+
+   
+
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 forwardMovement = transform.forward * vertical;
+        Vector3 rightMovement = transform.right * horizontal;
+        transform.position += (forwardMovement + rightMovement).normalized * Time.deltaTime * speedCamera;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            RotateCamera();
+        }
+    }
+
+    private void RotateCamera()
+    {
+        Debug.Log("estou rotacionando a camera");
+
+        camera.transform.RotateAround( Vector3.up, speedCamera * Time.deltaTime);
+
+        //camera.transform.rotation = 
+    }
+}
