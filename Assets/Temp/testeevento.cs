8003cdd678ba0ad8064145acfb39a2fca9c7be80
Renato <renatofelixmartins@gmail.com>
2020-12-06 21:25:59 -0300

    add comercio grande

---
+++ Assets/Temp/testeevento.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+
+     
+    public class testeevento : MonoBehaviour
+    {
+
+        private void OnEnable()
+        {
+            GridManager.Instance.OnSelectTile += TESTESTESTE;
+        }
+
+        private void OnDisable()
+        {
+            GridManager.Instance.OnSelectTile -= TESTESTESTE;
+        }
+
+        public void TESTESTESTE(Structure e)
+        {
+            Debug.Log("ALGO FOI SELECIONADO");
+        }
+    }
+}
